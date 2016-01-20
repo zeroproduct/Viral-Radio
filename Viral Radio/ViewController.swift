cdbd3f5f5b239766681834bc2159596fc0c1929e
Daniel Rica <Daniel@daniels-macbook-pro.local>
2016-01-20 15:40:55 -0800

    Initial Commit

---
+++ Viral Radio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Viral Radio
+//
+//  Created by Daniel Rica on 1/20/16.
+//  Copyright © 2016 Viral. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
